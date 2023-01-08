c56141d9cef572c1db4a68fec336acf3009df9bd
Victor Trinh <victor.trinh@gsoft.com>
2023-01-08 16:33:59 -0500

    Added dark mode switcher

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,22 @@
 import React, { ReactNode } from "react";
+import { ModeToggle } from "./ModeToggle";
+import { Container } from "./design-system";
 
 type Props = {
   children: ReactNode;
 };
 
 const Layout = ({ children }: Props) => (
-  <div className="py-8 min-h-screen bg-slate-100 dark:bg-background">
+  <div className="min-h-screen bg-slate-100 dark:bg-background">
+    <div className="w-full h-12 mb-6 flex items-center bg-slate-300 dark:bg-slate-800">
+      <Container className="flex items-center justify-between">
+        <div className="flex items-end font-serif dark:text-white font-extrabold text-3xl tracking-tighter">
+          <div>10k</div>
+          <div className="ml-2 uppercase text-xl">stay hard</div>
+        </div>
+        <ModeToggle />
+      </Container>
+    </div>
     <div>{children}</div>
   </div>
 );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,21 @@ import { Inter } from "@next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/global.css";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <main className={`${inter.className} font-sans`}>
-      <Component {...pageProps} />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        closeOnClick
-        draggable
-        theme="dark"
-      />
-    </main>
-  );
-};
+const App = ({ Component, pageProps }: AppProps) => (
+  <main className={`${inter.className} font-sans`}>
+    <Component {...pageProps} />
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      closeOnClick
+      draggable
+      theme="dark"
+    />
+  </main>
+);
 
 export default App;
