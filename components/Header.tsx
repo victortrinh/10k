630ce14f3695ec7a4bb07dd24dea6aed32c36571
Victor Trinh <victor.trinh@gsoft.com>
2023-01-09 12:04:25 -0500

    Add logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,34 @@
 import React from "react";
 import { Container } from "./design-system";
 import { ModeToggle } from "./ModeToggle";
+import { useTheme } from "next-themes";
+import Image from "next/image";
 
-const Header: React.FC = () => (
-  <div className="w-full h-[60px] mb-6 flex items-center">
-    <Container className="h-full">
-      <div className="w-full h-full  border-b border-gray-800 flex items-center justify-between">
-        <div className="flex items-end font-serif dark:text-white font-extrabold text-3xl tracking-tighter">
-          <div>10k</div>
-          <div className="ml-2 uppercase text-xl">stay hard</div>
+const Header: React.FC = () => {
+  const { theme } = useTheme();
+
+  const logoSize = 40;
+
+  let logo = (
+    <Image src="/images/logo-white.png" alt="10k" width={40} height={40} />
+  );
+
+  if (theme === "light") {
+    logo = (
+      <Image src="/images/logo-black.png" alt="10k" width={40} height={40} />
+    );
+  }
+
+  return (
+    <div className="w-full h-[60px] mb-6 flex items-center">
+      <Container className="h-full">
+        <div className="w-full h-full  border-b border-gray-800 flex items-center justify-between">
+          {logo}
+          <ModeToggle />
         </div>
-        <ModeToggle />
-      </div>
-    </Container>
-  </div>
-);
+      </Container>
+    </div>
+  );
+};
 
 export default Header;
