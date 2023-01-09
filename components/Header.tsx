4e134f0f6f98b3f25e0ffb7f9e6be6401475d873
Victor Trinh <victor.trinh@gsoft.com>
2023-01-09 09:40:48 -0500

    Fix background color in notches for each color mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { Container } from "./design-system";
+import { ModeToggle } from "./ModeToggle";
 
-const Header: React.FC = () => {
-  const router = useRouter();
-  const isActive: (pathname: string) => boolean = (pathname) =>
-    router.pathname === pathname;
-
-  let left = (
-    <Link href="/">
-      <a className="font-bold" data-active={isActive("/")}>
-        Home
-      </a>
-    </Link>
-  );
-
-  return (
-    <nav className="shadow-sm flex h-[60px] items-center mb-8">
-      <div className="container">{left}</div>
-    </nav>
-  );
-};
+const Header: React.FC = () => (
+  <div className="w-full h-[60px] mb-6 flex items-center">
+    <Container className="h-full">
+      <div className="w-full h-full  border-b border-gray-800 flex items-center justify-between">
+        <div className="flex items-end font-serif dark:text-white font-extrabold text-3xl tracking-tighter">
+          <div>10k</div>
+          <div className="ml-2 uppercase text-xl">stay hard</div>
+        </div>
+        <ModeToggle />
+      </div>
+    </Container>
+  </div>
+);
 
 export default Header;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from "react";
-import { ModeToggle } from "./ModeToggle";
-import { Container } from "./design-system";
+import Header from "./Header";
 
 type Props = {
   children: ReactNode;
@@ -8,15 +7,7 @@ type Props = {
 
 const Layout = ({ children }: Props) => (
   <div className="min-h-screen bg-slate-100 dark:bg-background">
-    <div className="w-full h-12 mb-6 flex items-center bg-slate-300 dark:bg-slate-800">
-      <Container className="flex items-center justify-between">
-        <div className="flex items-end font-serif dark:text-white font-extrabold text-3xl tracking-tighter">
-          <div>10k</div>
-          <div className="ml-2 uppercase text-xl">stay hard</div>
-        </div>
-        <ModeToggle />
-      </Container>
-    </div>
+    <Header />
     <div>{children}</div>
   </div>
 );
