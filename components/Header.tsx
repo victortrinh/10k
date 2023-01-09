ea4797e4030ccaf820393ffdf81a64923d354825
Victor Trinh <victor.trinh@gsoft.com>
2023-01-09 12:49:11 -0500

    Fix console error

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Container } from "./design-system";
 import { ModeToggle } from "./ModeToggle";
 import { useTheme } from "next-themes";
-import Image from "next/image";
 
 const Header: React.FC = () => {
   const { theme } = useTheme();
@@ -10,25 +9,23 @@ const Header: React.FC = () => {
   const logoSize = 40;
 
   let logo = (
-    <Image
+    <img
       className="w-10 h-auto"
       src="/images/logo-white.png"
       alt="10k"
       width={logoSize}
       height={logoSize}
-      priority
     />
   );
 
   if (theme === "light") {
     logo = (
-      <Image
+      <img
         className="w-10 h-auto"
         src="/images/logo-black.png"
         alt="10k"
         width={logoSize}
         height={logoSize}
-        priority
       />
     );
   }
@@ -36,7 +33,7 @@ const Header: React.FC = () => {
   return (
     <div className="w-full h-[60px] mb-6 flex items-center">
       <Container className="h-full">
-        <div className="w-full h-full  border-b border-gray-800 flex items-center justify-between">
+        <div className="w-full h-full relative border-b border-gray-800 flex items-center justify-between">
           {logo}
           <ModeToggle />
         </div>
